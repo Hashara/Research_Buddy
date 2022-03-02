a5b75d4cc067710e31b91b4f308ccc6755000ace
hashara <kumarasinghe.k.a.s.h@gmail.com>
2022-03-02 09:35:38 +0530

    init views

--- app/src/main/java/com/example/researchbuddy/LoginActivity.java
+++ app/src/main/java/com/example/researchbuddy/LoginActivity.java
@@ -3,12 +3,32 @@ package com.example.researchbuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout txtLayoutEmail;
+    private TextInputLayout txtLayoutPassword;
+
+    private Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initViews();
+    }
+
+    private void initViews() {
+        txtLayoutEmail = findViewById(R.id.txtLayoutEmail);
+        txtLayoutPassword = findViewById(R.id.txtLayoutPassword);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+
     }
 }
