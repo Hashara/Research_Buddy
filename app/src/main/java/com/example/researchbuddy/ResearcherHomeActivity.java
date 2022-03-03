a1b7e75995303b4181a298bf13ac28f65b5bf1cc
hashara <kumarasinghe.k.a.s.h@gmail.com>
2022-03-03 13:15:44 +0530

    researcher menu

--- app/src/main/java/com/example/researchbuddy/ResearcherHomeActivity.java
+++ app/src/main/java/com/example/researchbuddy/ResearcherHomeActivity.java
@@ -3,6 +3,9 @@ package com.example.researchbuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class ResearcherHomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class ResearcherHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_researcher_home);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.researcher_main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_add_project:
+//                openSearch();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
