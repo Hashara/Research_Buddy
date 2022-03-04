09e633c84c3982dc6724a82c626b264c8bc9d941
hashara <kumarasinghe.k.a.s.h@gmail.com>
2022-03-04 20:01:12 +0530

    menu bar

--- app/src/main/java/com/example/researchbuddy/component/participant/ParticipantHomeActivity.java
+++ app/src/main/java/com/example/researchbuddy/component/participant/ParticipantHomeActivity.java
@@ -7,10 +7,18 @@ import android.os.Bundle;
 import com.example.researchbuddy.R;
 
 public class ParticipantHomeActivity extends AppCompatActivity {
+    private static final String TAG = "ResearcherHomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_participant_home);
+
+        //action bar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
     }
 }
