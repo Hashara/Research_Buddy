5ddddbbb96c9185eaaf4d91ec0034c68d51b8e99
hashara <kumarasinghe.k.a.s.h@gmail.com>
2022-03-10 14:37:45 +0530

    questionnaires UI

---
+++ app/src/main/java/com/example/researchbuddy/component/researcher/QuestionnairePageActivity.java
@@ -0,0 +1,16 @@
+package com.example.researchbuddy.component.researcher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.researchbuddy.R;
+
+public class QuestionnairePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionnaries);
+    }
+}
