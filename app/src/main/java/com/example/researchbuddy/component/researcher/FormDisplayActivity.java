b183312812999e77a0892b5a9a64300680be18ce
hashara <kumarasinghe.k.a.s.h@gmail.com>
2022-03-16 12:16:30 +0530

    firm display title and description

--- app/src/main/java/com/example/researchbuddy/component/researcher/FormDisplayActivity.java
+++ app/src/main/java/com/example/researchbuddy/component/researcher/FormDisplayActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.example.researchbuddy.R;
 import com.example.researchbuddy.model.FormModel;
@@ -13,14 +14,29 @@ public class FormDisplayActivity extends AppCompatActivity {
     private FormModel form;
     private String TAG = "FormDisplayActivity";
 
+    private TextView txt_form_title;
+    private TextView txt_form_description;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_display);
 
         Bundle bundle = getIntent().getExtras();
-        if (bundle != null)
+        if (bundle != null) {
             form = (FormModel) getIntent().getSerializableExtra("form");
+            initViews();
+        }
         Log.d(TAG, form.toString());
+
+    }
+
+    private void initViews() {
+        txt_form_title = findViewById(R.id.txt_form_title);
+        txt_form_description = findViewById(R.id.txt_from_description);
+
+        txt_form_title.setText(form.getTitle());
+        txt_form_description.setText(form.getDescription());
+
     }
 }
