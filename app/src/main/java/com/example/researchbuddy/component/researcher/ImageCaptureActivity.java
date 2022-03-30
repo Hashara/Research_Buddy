5bddfa39656d5f9db46ffc42744cfd05e256b882
hashara <kumarasinghe.k.a.s.h@gmail.com>
2022-03-30 20:06:08 +0530

    image save high quality

--- app/src/main/java/com/example/researchbuddy/component/researcher/ImageCaptureActivity.java
+++ app/src/main/java/com/example/researchbuddy/component/researcher/ImageCaptureActivity.java
@@ -8,9 +8,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -27,6 +29,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.loader.content.CursorLoader;
 
 import com.example.researchbuddy.R;
@@ -45,6 +48,7 @@ import java.util.Date;
 import java.util.Objects;
 
 public class ImageCaptureActivity extends AppCompatActivity {
+
     private String TAG = "ImageCaptureActivity";
 
 
@@ -53,6 +57,7 @@ public class ImageCaptureActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
     private ImageView imageView;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    private String currentPhotoPath;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,28 +72,37 @@ public class ImageCaptureActivity extends AppCompatActivity {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
             } else {
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+
             }
+
+            File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+            try {
+                File imageFile = File.createTempFile(getFileName(), "jpg", storageDirectory);
+
+                currentPhotoPath = imageFile.getAbsolutePath();
+
+                Uri uri = FileProvider.getUriForFile(ImageCaptureActivity.this,
+                        "com.example.researchbuddy.fileprovider", imageFile);
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+            } catch (Exception e) {
+
+            }
+
         }
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
-            Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
-            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(cameraIntent, CAMERA_REQUEST);
-        }
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
+            Bitmap photo = BitmapFactory.decodeFile(currentPhotoPath);
+            imageView.setImageBitmap((Bitmap) data.getExtras().get("data"));
 
             String imageFileName = getFileName() + ".png";
 
@@ -103,38 +117,38 @@ public class ImageCaptureActivity extends AppCompatActivity {
                 photo.compress(Bitmap.CompressFormat.PNG, 100, bos);
             } catch (Exception ex) {
                 ex.printStackTrace();
-            }
-
-            if (bos != null) {
-                try {
-                    bos.flush();
-                    //bos.close();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-
-            if (bos != null) {
-                try {
-                    bos.close();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-
-
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
+            } finally {
+                if (bos != null) {
+                    try {
+                        bos.flush();
+                        //bos.close();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
                 }
 
+                if (bos != null) {
+                    try {
+                        bos.close();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+
+
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+
+                }
+
+                bos = null;
+                fos = null;
+                finish();
             }
-
-            bos = null;
-            fos = null;
-
 
         }
     }
@@ -144,6 +158,5 @@ public class ImageCaptureActivity extends AppCompatActivity {
         Date dt = new Date();
         return sdf.format(dt);
     }
-
 
 }
